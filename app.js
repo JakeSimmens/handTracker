995f43bdbcc8c7a49fa791362e66df8d1fcd552c
Jake Simmens <rocknjake@gmail.com>
2020-06-25 14:56:07 -0500

    Hand detection works with hand boxes commented out in code. Start section to add sounds based on hand locations.

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
 const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
+let loadModel;
 
-//start video feed from web cam
-handTrack.startVideo(video)
-    .then(status => {
-        if (status) {
-            navigator.mediaDevices.getUserMedia(
-                { video: {} },
-                (stream) => {
-                    video.srcObject = stream;
-                },
-                (err) => {
-                    console.log("Video feed not working.");
-                }
-            )
-        }
-    });
-
-// Load the model.
-handTrack.load().then(model => {
-    // detect objects in the image.
-    model.detect(video).then(predictions => {
-        console.log('Predictions: ', predictions);
-    });
-});
 
 const modelParams = {
     flipHorizontal: true,   // flip e.g for video 
@@ -34,11 +12,85 @@ const modelParams = {
     scoreThreshold: 0.79,    // confidence threshold for predictions.
 }
 
-handTrack.load(modelParams).then(model => {
-    // model.detect(video).then(predictions => {
-    //     //console.log(predictions);
-    // });
+//access video feed based on browser
+navigator.getUserMedia = navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+
+//detect hands in video
+function runDetection() {
+    model.detect(video).then(predictions => {
+        console.log("Predictions: ", predictions);
+
+        //CODE TO INTERACT WITH SOUND WILL BE BASED ON PREDICTION DATA
+        //console.log(predictions.length);
+        if (predictions.length > 0) {
+            soundPlayer(predictions);
+        }
+
+
+
+        //draws second image with boxes
+        //model.renderPredictions(predictions, canvas, context, video);
+
+        //this will request the detection over and over again
+        //You can also use set interval int he startVideo function to call repeatedly
+        //requestAnimationFrame(runDetection);
+
+    });
+}
+
+//start video feed from web cam
+handTrack.startVideo(video)
+    .then(status => {
+        if (status) {
+            navigator.getUserMedia(
+                { video: {} },
+                (stream) => {
+                    video.srcObject = stream;
+
+                    setInterval(runDetection, 100);
+                },
+                (err) => {
+                    console.log("Video feed not working.");
+                }
+            )
+        }
+    });
+
+
+
+// Load the model.
+handTrack.load(modelParams).then(loadModel => {
+    model = loadModel;
+
 });
+
+function soundPlayer(objLocation) {
+    console.log(objLocation[0].bbox[1]);
+    console.log(objLocation[0].score);
+}
+
 
 // WebGL2RenderingContext.getBufferSubData()
 // o.getBufferSubData(p.ARRAY_BUFFER, 0, a)
+
+
+// bbox: (4) […]
+// ​​​
+// 0: 86.93609237670898
+// ​​​
+// 1: 187.18571662902832
+// ​​​
+// 2: 139.76024627685547
+// ​​​
+// 3: 171.50819778442383
+// ​​​
+// length: 4
+// ​​​
+// <prototype>: Array []
+// ​​
+// class: 0
+// ​​
+// score: 0.925246000289917

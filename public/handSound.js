9ba77a795d3dd5090fc7d7f38f2fa72808a659d4
Jake Simmens <rocknjake@gmail.com>
2020-06-29 21:14:20 -0500

    Refactor file structure and implement express server.

---
+++ public/handSound.js
@@ -0,0 +1,114 @@
+
+function drawMirroredVideo(video, canvas, context) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.width = video.width;
+    canvas.height = video.height;
+    context.save();
+    context.scale(-1, 1);  //flips to mirror image
+    context.translate(-video.width, 0);
+    context.drawImage(video, 0, 0, video.width, video.height);
+    context.restore();
+}
+
+function processPredictions(predictions, rectLocations) {
+
+    let centerX = 0;
+    let centerY = 0;
+
+    if (!predictions.length) {
+        return;
+    }
+
+    //pulls out x,y coordinates to send to sound player
+    for (let prediction of predictions) {
+
+        //figure center coordinates of hand box
+        centerX = prediction.bbox[0] + prediction.bbox[2] * .5;
+        centerY = prediction.bbox[1] + prediction.bbox[3] * .5;
+
+        //draws white dot for hand location
+        context.fillStyle = "white";
+        context.fillRect(centerX, centerY, 5, 5);
+
+        soundPlayer(centerX, centerY, rectLocations);
+    }
+
+
+}
+
+function overlayRectangles(video, context) {
+
+    //figure rectangle size and spacing based on video width
+    let rectWidth = video.width * .3;
+    let rectHeight = rectWidth * .75;
+    let xSpacing = (video.width - (rectWidth * 2)) / 3;
+    let ySpacing = xSpacing * .75;
+    let rightBoxSpacing = 0;
+    let botBoxSpacing = 0;
+    let rectLocations = {};
+
+    //upper left
+    rectLocations = {
+        upperLeft: [xSpacing, ySpacing, rectWidth, rectHeight]
+    };
+    drawRect("rgba(0,255,100, .5)", rectLocations.upperLeft, context);
+
+    //upper right
+    rightBoxSpacing = xSpacing * 2 + rectWidth;
+    rectLocations.upperRight = [rightBoxSpacing, ySpacing, rectWidth, rectHeight];
+    drawRect("rgba(255,150,0, .5)", rectLocations.upperRight, context);
+
+    //lower left
+    botBoxSpacing = ySpacing * 2 + rectHeight;
+    rectLocations.lowerLeft = [xSpacing, botBoxSpacing, rectWidth, rectHeight];
+    drawRect("rgba(128, 0, 128, .5)", rectLocations.lowerLeft, context);
+
+    //lower right
+    rectLocations.lowerRight = [rightBoxSpacing, botBoxSpacing, rectWidth, rectHeight];
+    drawRect("rgba(0,0,255, .5)", rectLocations.lowerRight, context);
+
+    return rectLocations;
+}
+
+function drawRect(color, rect, context) {
+    context.fillStyle = color;
+    context.fillRect(rect[0], rect[1], rect[2], rect[3]);
+}
+
+function soundPlayer(x, y, locations) {
+
+    const { upperLeft, lowerLeft, upperRight, lowerRight } = locations;
+
+    //box arrays:  xStart, yStart, xWidth, yHeight
+    let upperTopEdge = upperLeft[1];
+    let upperBotEdge = upperLeft[1] + upperLeft[3];
+    let lowerTopEdge = lowerLeft[1];
+    let lowerBotEdge = lowerLeft[1] + lowerLeft[3];
+    let leftBoxLeftEdge = upperLeft[0];
+    let leftBoxRightEdge = upperLeft[0] + upperLeft[2];
+    let rightBoxLeftEdge = upperRight[0];
+    let rightBoxRightEdge = upperRight[0] + upperRight[2];
+
+
+    if (y > upperTopEdge && y < upperBotEdge) {
+        //top boxes
+        if (x > leftBoxLeftEdge && x < leftBoxRightEdge) {
+            //play upper left sound
+            audio1.play();
+        } else if (x > rightBoxLeftEdge && x < rightBoxRightEdge) {
+            //play upper right sound
+            audio2.play();
+        }
+    } else if (y > lowerTopEdge && y < lowerBotEdge) {
+        //bottom boxes
+        if (x > leftBoxLeftEdge && x < leftBoxRightEdge) {
+            //play lower left sound
+            audio3.play();
+        } else if (x > rightBoxLeftEdge && x < rightBoxRightEdge) {
+            //play lower right sound
+            audio4.play();
+        }
+    }
+
+
+}
---
+++ public/loadHand.js
@@ -0,0 +1,88 @@
+// @article{Dibia2017,
+//     author = {Victor, Dibia},
+//     title = {HandTrack: A Library For Prototyping Real-time Hand TrackingInterfaces using Convolutional Neural Networks},
+//     year = {2017},
+//     publisher = {GitHub},
+//     journal = {GitHub repository},
+//     url = {https://github.com/victordibia/handtracking/tree/master/docs/handtrack.pdf}, 
+//   }
+
+
+const video = document.getElementById("video");
+const audio1 = document.getElementById("audio1");
+const audio2 = document.getElementById("audio2");
+const audio3 = document.getElementById("audio3");
+const audio4 = document.getElementById("audio4");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+let loadModel;
+
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video 
+    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.79,    // confidence threshold for predictions.
+}
+
+//access video feed based on browser
+// navigator.getUserMedia = navigator.getUserMedia ||
+//     navigator.webkitGetUserMedia ||
+//     navigator.mozGetUserMedia ||
+//     navigator.msGetUserMedia;
+
+
+//detect hands in video
+function runDetection() {
+    model.detect(video)
+        .then(predictions => {
+
+            let overlayLocations;
+
+            drawMirroredVideo(video, canvas, context);
+
+            //Handtrack's method of drawing second image with boxes of hand locations
+            //model.renderPredictions(predictions, canvas, context, video);
+
+            overlayLocations = overlayRectangles(video, context);
+
+            //Code to interact wtih sound is based on prediction data
+            if (predictions.length > 0) {
+                processPredictions(predictions, overlayLocations);
+            }
+
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+//start video feed from web cam
+handTrack.startVideo(video)
+    .then(status => {
+        if (status) {
+            navigator.mediaDevices.getUserMedia(
+                {
+                    video: {
+                        width: 640
+                    }
+                })
+                .then(stream => {
+                    video.srcObject = stream;
+                    setInterval(runDetection, 500);
+                });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+// Load the model.
+handTrack.load(modelParams)
+    .then(loadModel => {
+        model = loadModel;
+    })
+    .catch(err => {
+        console.log(err);
+    });
